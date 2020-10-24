1a5b0750bddeb98907f4ed34a9cd623f873a8ed3
FilipKramar <filip.kramar4637@gmail.com>
2020-10-24 17:21:01 +0200

    Initial commit: dz1 finished

---
+++ DZ1/Service/Episode.cs
@@ -0,0 +1,41 @@
+﻿namespace Service
+{
+    public class Episode
+    {
+        private int ViewerCount;
+        private double TotalSum;
+        private double MaxScore;
+        
+        public Episode() { }
+
+        public Episode(int viewerCount, double totalScore, double maxScore)
+        {
+            ViewerCount = viewerCount;
+            TotalSum = totalScore;
+            MaxScore = maxScore;
+        }
+
+        public void AddView(double score)
+        {
+            ViewerCount = ViewerCount + 1;
+            this.TotalSum = TotalSum + score;
+            if (score > this.MaxScore)
+                MaxScore = score;
+        }
+
+        public double GetMaxScore()
+        {
+            return MaxScore;
+        }
+
+        public double GetAverageScore()
+        {
+            return TotalSum/ViewerCount;
+        }
+
+        public int GetViewerCount()
+        {
+            return ViewerCount;
+        }
+    }
+}
